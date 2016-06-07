5dd61f2d0b97b77cb1c2044bec009f169bfa69f7
Dieter Hubau <dhubau@gmail.com>
2016-06-07 09:23:54 +0200

    Working example
    This starts up and works

--- src/main/java/com/example/DataJpaPreAuthorizeApplication.java
+++ src/main/java/com/example/DataJpaPreAuthorizeApplication.java
@@ -1,34 +1,19 @@
 package com.example;
 
-import com.example.comment.Comment;
-import com.example.comment.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.Collections;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @SpringBootApplication
-public class DataJpaPreAuthorizeApplication implements CommandLineRunner {
-
-    @Autowired
-    private CommentRepository commentRepository;
+public class DataJpaPreAuthorizeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaPreAuthorizeApplication.class, args);
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-        SecurityContext ctx = SecurityContextHolder.createEmptyContext();
-        SecurityContextHolder.setContext(ctx);
-        ctx.setAuthentication(new UsernamePasswordAuthenticationToken("dhubau", "password", Collections.singletonList(new SimpleGrantedAuthority("USER"))));
-        commentRepository.save(new Comment("dhubau", "foo"));
-        SecurityContextHolder.clearContext();
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("dhubau").password("password").roles("USER");
     }
 }
--- src/main/java/com/example/security/WebSecurityConfig.java
+++ src/main/java/com/example/security/WebSecurityConfig.java
@@ -1,15 +1,26 @@
 package com.example.security;
 
+import com.example.comment.Comment;
+import com.example.comment.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collections;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements CommandLineRunner {
+
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,8 +36,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Autowired
-    public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("dhubau").password("password").roles("USER");
+    @Override
+    public void run(String... strings) throws Exception {
+        SecurityContext ctx = SecurityContextHolder.createEmptyContext();
+        SecurityContextHolder.setContext(ctx);
+        ctx.setAuthentication(new UsernamePasswordAuthenticationToken("dhubau", "password", Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))));
+        commentRepository.save(new Comment("dhubau", "foo"));
+        SecurityContextHolder.clearContext();
     }
 }
